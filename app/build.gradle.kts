f38ea306e0d3e8f15267fa36b58e3924e092b2e2
fxanhkhoa <fxanhkhoa@gmail.com>
2026-03-16 07:47:08 +0700

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.fxanhkhoa.what_to_eat_android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
-        versionName = "2"
+        versionCode = 3
+        versionName = "3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
