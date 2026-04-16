277c9d44ffdfbd7fbe736be8e737496368004000
fxanhkhoa <fxanhkhoa@gmail.com>
2026-04-16 16:02:12 +0700

    v6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.fxanhkhoa.what_to_eat_android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
-        versionName = "5"
+        versionCode = 6
+        versionName = "6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
